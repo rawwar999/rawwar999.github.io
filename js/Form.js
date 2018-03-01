94e777e000d6397401f31d1c8c571d3e9852756a
rawwar999 <rawwar999@gmail.com>
2018-03-01 23:49:09 +0200

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,15 @@
+var menuOpen = document.querySelector('.head__menu');
+var controlMenu = document.querySelector('.menu-header');
+var menuClose = document.querySelector('.menu__close');
+
+menuOpen.addEventListener("click", function(event) {
+  event.preventDefault();
+  controlMenu.classList.remove('menu__header--close');
+});
+
+menuClose.addEventListener("click", function(event) {
+  event.preventDefault();
+  controlMenu.classList.add('menu__header--close');
+});
+
+
